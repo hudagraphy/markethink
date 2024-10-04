0ebc9c88f3e4026b0d9bc9f12c22d8b7dde32640
Wachiddin M Huda <andezyoung@gmail.com>
2024-10-04 22:14:16 +0700

    Manual Stepper Done

--- lib/addAgenda.dart
+++ lib/addAgenda.dart
@@ -11,88 +11,99 @@ class InputAgenda extends StatefulWidget {
 }
 
 class _InputAgendaState extends State<InputAgenda> {
+  int step = 1;
+  final _kunciForm = GlobalKey<FormBuilderState>();
   @override
   Widget build(BuildContext context) {
     double lebarLayar = MediaQuery.of(context).size.width;
     double tinggiLayar = MediaQuery.of(context).size.height;
-    final _kunciForm = GlobalKey<FormBuilderState>();
+    
+    
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.symmetric(
-            horizontal: lebarLayar <= 720
-                ? (5 / 100 * lebarLayar)
-                : (25 / 100 * lebarLayar)),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //title
-              Container(
-                padding: EdgeInsets.all(5),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    color: Colors.white),
-                child: Text("Agenda Baru"),
-              ),
-              //mainForm
-              Container(
-                clipBehavior: Clip.hardEdge,
-                height: 400,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
+      body: FormBuilder(
+        key: _kunciForm,
+        child: Container(
+          padding: EdgeInsets.symmetric(
+              horizontal: lebarLayar <= 720
+                  ? (5 / 100 * lebarLayar)
+                  : (25 / 100 * lebarLayar)),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //title
+                Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.white),
+                  child: Text("Agenda Baru"),
                 ),
-                child: Stack(
-                  children: [
-                    //tombol tombol
-                    Row(
-                      children: [
-                        //tombol balik
-                        Visibility(
-                          visible: true,
-                          child: Expanded(
-                            child: Container(
-                              padding: EdgeInsets.all(20),
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                              ),
-                              child: Align(
-                                alignment: Alignment.bottomCenter,
-                                child: Text(
-                                  "Balik",
-                                  style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.blue),
+                //mainForm
+                Container(
+                  clipBehavior: Clip.hardEdge,
+                  height: 400,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Stack(
+                    children: [
+                      //tombol tombol
+                      Row(
+                        children: [
+                          //tombol balik
+                          Visibility(
+                            visible: step > 1,
+                            child: Expanded(
+                              child: GestureDetector(
+                                onTap: () => setState(() {
+                                  step--;
+                                }),
+                                child: Container(
+                                  padding: EdgeInsets.all(20),
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                  ),
+                                  child: Align(
+                                    alignment: Alignment.bottomCenter,
+                                    child: Text(
+                                      "Balik",
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.blue),
+                                    ),
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                        //tombol lanjut
-                        Expanded(
-                          child: Container(
-                            padding: EdgeInsets.all(20),
-                            decoration: BoxDecoration(
-                              color: Colors.blue,
-                            ),
-                            child: Align(
-                              alignment: Alignment.bottomCenter,
-                              child: Text(
-                                "Lanjut",
-                                style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white),
+                          //tombol lanjut
+                          Expanded(
+                            child: GestureDetector(
+                              onTap: () => setState(() {step++; debugPrint(step.toString());}),
+                              child: Container(
+                                padding: EdgeInsets.all(20),
+                                decoration: BoxDecoration(
+                                  color: Colors.blue,
+                                ),
+                                child: Align(
+                                  alignment: Alignment.bottomCenter,
+                                  child: Text(
+                                    "Lanjut",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white),
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
-                        )
-                      ],
-                    ),
-                    //form input
-                    FormBuilder(
-                      key: _kunciForm,
-                      child: Container(
+                          )
+                        ],
+                      ),
+                      //form input
+                      Container(
                         height: 330,
                         padding: EdgeInsets.all(20),
                         decoration: BoxDecoration(
@@ -106,44 +117,67 @@ class _InputAgendaState extends State<InputAgenda> {
                           ],
                         ),
                         child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Column(
-                              children: [
-                                //field Mangkate?
-                                FormBuilderDateTimePicker(
-                                  name: "waktuAgendaStart",
-                                  firstDate: DateTime.now(),
-                                  lastDate: DateTime(2024, 12, 31),
-                                  decoration: PengaturanDekorasiField(
-                                      hintText: "Mangkate?"),
-                                  onChanged: (value) {
-                                    _kunciForm.currentState?.save();
-                                    debugPrint(_kunciForm.currentState?.value
-                                        .toString());
-                                    setState(() {});
-                                  },
-                                ),
-                                //field Balikke?
-                                FormBuilderDateTimePicker(
-                                  name: "waktuAgendaEnd",
-                                  //enabled: ,
-                                  firstDate: _kunciForm
-                                      .currentState?.value['waktuAgendaStart'],
-                                  lastDate: DateTime(2024, 12, 31),
-                                  decoration: PengaturanDekorasiField(
-                                      hintText: "Balikke?"),
-                                ),
-                              ],
+                            //Step 1
+                            Visibility(
+                              visible: step == 1,
+                              child: Column(
+                                children: [
+                                  //fieldMangkate
+                                  FormBuilderDateTimePicker(
+                                    name: "waktuAgendaStart",
+                                    firstDate: DateTime.now(),
+                                    lastDate: DateTime(2024, 12, 31),
+                                    decoration: PengaturanDekorasiField(
+                                      hintText: "Mangkate?",
+                                    ),
+                                    onChanged: (value) {
+                                      _kunciForm.currentState?.save();
+                                      debugPrint(_kunciForm.currentState?.value
+                                          .toString());
+                                      setState(() {});
+                                    },
+                                  ),
+                                  SizedBox(
+                                    height: 20,
+                                  ),
+                                  //field Balikke?
+                                  FormBuilderDateTimePicker(
+                                    name: "waktuAgendaEnd",
+                                    //enabled: ,
+                                    firstDate: _kunciForm.currentState
+                                        ?.value['waktuAgendaStart'],
+                                    lastDate: DateTime(2024, 12, 31),
+                                    decoration: PengaturanDekorasiField(
+                                        hintText: "Balikke?"),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            //Step 2
+                            Visibility(
+                              visible: step == 2,
+                              child: Column(
+                                children: [
+                                  //jenis Agenda
+                                  FormBuilderDropdown(
+                                    name: "jenisAgenda",
+                                    decoration: PengaturanDekorasiField(
+                                        hintText: "Jenis Agenda"),
+                                    items: [],
+                                  )
+                                ],
+                              ),
                             ),
                           ],
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
