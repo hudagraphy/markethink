6e6081e152cdf7892d2d130383b281c412d1fc1f
hudagraphy <andezyoung@gmail.com>
2024-10-04 17:50:10 +0700

    Form builder, Date Time Picker, dan input decoration

--- lib/addAgenda.dart
+++ lib/addAgenda.dart
@@ -1,13 +1,21 @@
 import 'package:easy_stepper/easy_stepper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:intl/intl.dart';
 
-class InputAgenda extends StatelessWidget {
+class InputAgenda extends StatefulWidget {
   const InputAgenda({super.key});
 
+  @override
+  State<InputAgenda> createState() => _InputAgendaState();
+}
+
+class _InputAgendaState extends State<InputAgenda> {
   @override
   Widget build(BuildContext context) {
     double lebarLayar = MediaQuery.of(context).size.width;
     double tinggiLayar = MediaQuery.of(context).size.height;
+    final _kunciForm = GlobalKey<FormBuilderState>();
     return Scaffold(
       body: Container(
         padding: EdgeInsets.symmetric(
@@ -18,6 +26,7 @@ class InputAgenda extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              //title
               Container(
                 padding: EdgeInsets.all(5),
                 decoration: BoxDecoration(
@@ -25,6 +34,7 @@ class InputAgenda extends StatelessWidget {
                     color: Colors.white),
                 child: Text("Agenda Baru"),
               ),
+              //mainForm
               Container(
                 clipBehavior: Clip.hardEdge,
                 height: 400,
@@ -80,10 +90,12 @@ class InputAgenda extends StatelessWidget {
                       ],
                     ),
                     //form input
-                    Container(
-                      height: 330,
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
+                    FormBuilder(
+                      key: _kunciForm,
+                      child: Container(
+                        height: 330,
+                        padding: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
                           color: Colors.white,
                           borderRadius: BorderRadius.circular(30),
                           boxShadow: [
@@ -91,7 +103,42 @@ class InputAgenda extends StatelessWidget {
                               color: Colors.black12,
                               blurRadius: 30,
                             )
-                          ]),
+                          ],
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Column(
+                              children: [
+                                //field Mangkate?
+                                FormBuilderDateTimePicker(
+                                  name: "waktuAgendaStart",
+                                  firstDate: DateTime.now(),
+                                  lastDate: DateTime(2024, 12, 31),
+                                  decoration: PengaturanDekorasiField(
+                                      hintText: "Mangkate?"),
+                                  onChanged: (value) {
+                                    _kunciForm.currentState?.save();
+                                    debugPrint(_kunciForm.currentState?.value
+                                        .toString());
+                                    setState(() {});
+                                  },
+                                ),
+                                //field Balikke?
+                                FormBuilderDateTimePicker(
+                                  name: "waktuAgendaEnd",
+                                  //enabled: ,
+                                  firstDate: _kunciForm
+                                      .currentState?.value['waktuAgendaStart'],
+                                  lastDate: DateTime(2024, 12, 31),
+                                  decoration: PengaturanDekorasiField(
+                                      hintText: "Balikke?"),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ],
                 ),
@@ -101,5 +148,20 @@ class InputAgenda extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  InputDecoration PengaturanDekorasiField({
+    required String hintText,
+  }) {
+    return InputDecoration(
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: BorderSide.none,
+        ),
+        contentPadding: EdgeInsets.all(25),
+        hintText: hintText,
+        hintStyle: TextStyle(fontWeight: FontWeight.bold),
+        fillColor: Colors.blue.shade50,
+        filled: true);
   }
 }
